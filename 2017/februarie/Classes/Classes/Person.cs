f86cffdd9d1bd8d6e1fecc3c2eb950de1f856816
Claudiu P. <claupcv@yahoo.com>
2017-03-19 21:56:28 +0200

    Classes Clau

---
+++ 2017/februarie/Classes/Classes/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Person
+    {
+        public string PersonName = string.Empty;
+
+        public static int PopCount = 0;
+
+        public void PrintPopCount()
+        {
+            // accesam membru static pt. citire
+            Console.WriteLine("Population count is: " + Person.PopCount);
+        }
+
+        public void DoSomethingWithPopulationCount()
+        {
+            // accesam membru static pt. scriere
+            Person.PopCount = 100;
+        }
+
+
+    }
+}
---
+++ 2017/februarie/Classes/Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Membrii de instanta si membrii statici
+            // instance
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person1.PrintPopCount();
+            person2.PrintPopCount();
+
+            person1.DoSomethingWithPopulationCount();
+
+            person1.PrintPopCount();
+            person2.PrintPopCount();
+
+
+
+            Person.PopCount++;
+            Console.WriteLine("Now the global population count raised to {0}", Person.PopCount);
+            Console.ReadKey();
+        }
+    }
+}
