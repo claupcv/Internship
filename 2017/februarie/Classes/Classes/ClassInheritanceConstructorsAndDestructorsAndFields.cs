35677075ca35e75a67744f7509373167ad6cf6ec
Clau <claupcv@yahoo.com>
2017-03-21 15:48:46 +0200

    Classes Clau

--- 2017/februarie/Classes/Classes/ClassInheritanceConstructorsAndDestructorsAndFields.cs
+++ 2017/februarie/Classes/Classes/ClassInheritanceConstructorsAndDestructorsAndFields.cs
@@ -8,6 +8,12 @@ namespace Classes
 {
     public class TestType
     {
+        // by ":this" cals the second constructor(runs the body) and next will run the first constructor body
+        public TestType(string context, int count) :this(context)
+        {
+            Console.WriteLine("TestType ctor in {0}={1}...", context,count);
+        }
+        
         public TestType(string context)
         {
             Console.WriteLine("TestType ctor in {0}...", context);
@@ -15,7 +21,7 @@ namespace Classes
     }
     public class BaseClass
     {
-        private readonly TestType testBase = new TestType("BaseClass");
+        private readonly TestType testBase = new TestType("BaseClass",2);
         public BaseClass()
         {
             Console.WriteLine("BaseClass ctor  ...");
@@ -29,7 +35,7 @@ namespace Classes
  
     public class DerivedClass : BaseClass
     {
-        private readonly TestType testDerived = new TestType("DerivedClass");
+        private readonly TestType testDerived = new TestType("DerivedClass",1);
         public DerivedClass()
         {
             Console.WriteLine("DerivedClass ctor ...");
