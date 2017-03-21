8d3237d86d723c5abc4c42d87afaf0a086048a57
Clau <claupcv@yahoo.com>
2017-03-21 15:51:37 +0200

    Classes Clau

---
+++ clau/2017/ianuarie/HelloWorld/HelloWorld/Indexers.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    using System.Runtime.InteropServices;
+
+    class Indexers
+    {
+        int iNumb;
+        string iName, iSurname;
+        private double iPrice;
+
+        public Indexers(int iNumb, string iName, string iSurname, double iPrice)
+        {
+            this.iNumb = iNumb;
+            this.iName = iName;
+            this.iSurname = iSurname;
+            this.iPrice = iPrice;
+        }
+
+        public object this[int indexes]
+        {
+            get
+            {
+                if (indexes == 0)
+                {
+                    return this.iNumb;
+                }
+                else if (indexes == 1)
+                {
+                    return this.iName;
+                }
+                else if (indexes == 2)
+                {
+                    return this.iSurname;
+                }
+                else if (indexes == 3)
+                {
+                    return this.iPrice;
+                }
+                return null;
+            }
+            set
+            {
+                if (indexes == 0)
+                {
+                    this.iNumb = (int)value;
+                }
+                else if (indexes == 1)
+                {
+                    this.iName = (string)value;
+                }
+                else if (indexes == 2)
+                {
+                    this.iSurname = (string)value;
+                }
+                else if (indexes == 3)
+                {
+                    this.iPrice = (double)value;
+                }
+            }
+        }
+
+    }
+
+    public static class  TestIndexers
+    {
+        public static void TestIndexersMethod()
+        {
+            Indexers index = new Indexers(1001, "Clau", "Pcv", 30.50);
+            Console.WriteLine("Number:" + index[0]);
+            Console.WriteLine("Name:" + index[1]);
+            Console.WriteLine("Surname:" + index[2]);
+            Console.WriteLine("Price:" + index[3]);
+            index[1] = "Temp";
+
+            Console.WriteLine("===========================");
+
+            Console.WriteLine("Number:" + index[0]);
+            Console.WriteLine("Name:" + index[1]);
+            Console.WriteLine("Surname:" + index[2]);
+            Console.WriteLine("Price:" + index[3]);
+
+        }
+    }
+
+}
---
+++ clau/2017/ianuarie/HelloWorld/HelloWorld/NestedTypes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class NestedTypes
+    {
+        public static int Nomber = 1;
+
+        public void test()
+        {
+            // call the member of inside class
+            Nested.numbersss = 1;
+        }
+        public class Nested
+        {
+            public static int numbersss;
+            public NestedTypes parent;
+
+            public Nested()
+            {
+            }
+            public Nested(NestedTypes parent)
+            {
+                this.parent = parent;
+
+                // calling the member of outside class
+                NestedTypes.Nomber = 2;
+            }
+        }
+    }
+    public static class RunNestedTypes
+    {
+        public static void RunNestedTypesMethod()
+        {
+            NestedTypes.Nested nest = new NestedTypes.Nested();
+        }
+        
+    }
+}
