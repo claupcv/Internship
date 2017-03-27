c7e3a0d63ac559e34c63e6ebef72ae6b7e509ef6
Claudiu P. <claupcv@yahoo.com>
2017-03-27 23:40:26 +0300

    Clau ProcessApp

---
+++ 2017/Martie/ProcessApp/ProcessApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessString processString = new ProcessString();
+            processString.Run(1);
+
+            //procesCore.Run(" 11");
+            //ConsoleInteratctiveMenu mainMenu = new ConsoleInteratctiveMenu();
+            //mainMenu.RunMenu();
+
+            Console.ReadKey();
+        }
+    }
+}
