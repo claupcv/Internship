ac0c93248171c39b55ce54a340367d389a4af048
flazar <florin.lazar@fortech.ro>
2017-04-04 10:15:22 +0300

    Minor refactoring (remove unused usings)

--- Courses/Apps/OOP/Classes/ProcessLibrary/ProcessApplication/Program.cs
+++ Courses/Apps/OOP/Classes/ProcessLibrary/ProcessApplication/Program.cs
@@ -1,10 +1,6 @@
 ﻿using ProcessApplication.Processes;
 using ProcessLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProcessApplication
 {
