4093f49129bf1087d9da3a2ad02b06b62ea0e469
Clau <claupcv@yahoo.com>
2017-04-14 16:42:48 +0300

    Clau

---
+++ clau/2017/aprilie/Curs11apr/Curs11apr/DynamicType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs11apr
+{
+    public static class DynamicType
+    {
+
+        public static dynamic CreateDinamic()
+        {
+            dynamic obj = new System.Dynamic.ExpandoObject();
+
+            obj.FirstName = "John";
+
+            obj.FirstName = 10 + obj.FirstName;
+
+            obj.SayHello = (Action)delegate() { Console.WriteLine("Hello"); };
+
+            return obj;
+        }
+
+        public static void ReadDinamic(dynamic obj)
+        {
+            Console.WriteLine(obj.FirstName);
+
+            obj.SayHello();
+        }
+    }
+}
