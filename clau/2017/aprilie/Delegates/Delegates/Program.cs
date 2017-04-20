26ceec0195ffd82526edf4d00e9ba0e23042d5f9
Clau <claupcv@yahoo.com>
2017-04-20 15:48:37 +0300

    Clau Delegates and NullTypes

---
+++ clau/2017/aprilie/Delegates/Delegates/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+
+        public delegate int Sum3Numbers(int a1, int a2, int a3);
+
+        public int CalculateSum3Numbers(int a1, int a2, int a3)
+        {
+            return a1 + a2 + a3;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Person person = new Person();
+            person.Run();
+
+            Console.WriteLine("================================");
+            DelegateExample delegateExample = new DelegateExample();
+            delegateExample.AddNum(10, 5);
+
+
+            AddNumDelegate adD = new AddNumDelegate(delegateExample.AddNum );
+            SayHelloDelegate say = new SayHelloDelegate(DelegateExample.SayHello);
+            string str = say("PCV"); 
+            
+
+            str = DelegateExample.SayHello("PCV");
+            Console.WriteLine(str);
+
+            Console.WriteLine("================================");
+
+            // ...
+            // si apoi undeva in codul aplicatiei
+            // ...
+
+            // define some observers
+            var observer1 = new IntegerCollectionChangeObserver("observer 1");
+            var observer2 = new IntegerCollectionChangeObserver("observer 2");
+
+            // define the observable collection
+            ObservableIntegerCollection col = new ObservableIntegerCollection();
+
+            // attach both observers
+            col.OnCollectionEvent += observer1.OnReceiveCollectionEvent;
+            col.OnCollectionEvent += observer2.OnReceiveCollectionEvent;
+
+            // make a change upon the collection and observe results
+            col.Add(1);
+            // Outputs:
+            // observer 1 > ElementAdded just happend, affected element is: 1
+            // observer 2 > ElementAdded just happend, affected element is: 1
+
+            // detach second observer
+            col.OnCollectionEvent -= observer2.OnReceiveCollectionEvent; ;
+
+            // make another change upon the collection and observe results
+            col.Update(0, 2);
+            // Outputs:
+            // observer 1 > ElementUpdated just happend, affected element is: 2
+
+            Console.ReadKey();
+
+        }
+    }
+}
